cfc619c89c1d2a21fd097bebba32c373ad10e369
Rick Conrad <richardjconrad@gmail.com>
2017-06-10 10:14:02 +0100

    Add RSSParser, download RSS feed and display first title

--- app/src/main/java/org/gospelcoding/dailydoseofgreek/VideoListActivity.java
+++ app/src/main/java/org/gospelcoding/dailydoseofgreek/VideoListActivity.java
@@ -2,6 +2,12 @@ package org.gospelcoding.dailydoseofgreek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
 
 public class VideoListActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class VideoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_list);
+
+        loadSomeRSS();
+    }
+
+    private void loadSomeRSS(){
+        String urlString = "http://dailydoseofgreek.com/feed";
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+            @Override
+            public void onTaskCompleted(ArrayList<Article> list) {
+                String title = list.get(0).getTitle();
+                TextView rssInfo = (TextView) findViewById(R.id.rssInfo);
+                rssInfo.setText(title);
+            }
+
+            @Override
+            public void onError() {
+                //log.e('rss error', 'Some error')
+            }
+        });
     }
 }
