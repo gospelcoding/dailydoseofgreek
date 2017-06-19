570b92d2323623865c8ebdf5b6fe6fdb84207d07
Rick Conrad <richardjconrad@gmail.com>
2017-06-19 22:16:48 +0100

    Import code from VimeoTest

---
+++ app/src/main/java/org/gospelcoding/dailydoseofgreek/HTML5WebView.java
@@ -0,0 +1,227 @@
+package org.gospelcoding.dailydoseofgreek;
+
+        import java.lang.reflect.Field;
+        import java.lang.reflect.Method;
+        import java.util.Timer;
+        import java.util.TimerTask;
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.os.Message;
+        import android.util.AttributeSet;
+        import android.util.Log;
+        import android.view.KeyEvent;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.Window;
+        import android.webkit.ConsoleMessage;
+        import android.webkit.GeolocationPermissions;
+        import android.webkit.WebChromeClient;
+        import android.webkit.WebSettings;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+        import android.widget.FrameLayout;
+
+public class HTML5WebView extends WebView {
+
+    private Context 							mContext;
+    private MyWebChromeClient					mWebChromeClient;
+    private View								mCustomView;
+    private FrameLayout							mCustomViewContainer;
+    private WebChromeClient.CustomViewCallback 	mCustomViewCallback;
+
+    private FrameLayout							mContentView;
+    private FrameLayout							mBrowserFrameLayout;
+    private FrameLayout							mLayout;
+
+    static final String TAG = "HTML5WebView";
+
+    private void init(Context context) {
+        mContext = context;
+        Activity a = (Activity) mContext;
+
+        mLayout = new FrameLayout(context);
+
+        mBrowserFrameLayout = (FrameLayout) LayoutInflater.from(a).inflate(R.layout.custom_screen, null);
+        mContentView = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.main_content);
+        mCustomViewContainer = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.fullscreen_custom_content);
+
+        mLayout.addView(mBrowserFrameLayout, COVER_SCREEN_PARAMS);
+
+        mWebChromeClient = new MyWebChromeClient();
+        setWebChromeClient(mWebChromeClient);
+
+        setWebViewClient(new MyWebViewClient());
+
+        // Configure the webview
+        WebSettings s = getSettings();
+        s.setBuiltInZoomControls(true);
+        s.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(true);
+        s.setSavePassword(true);
+        s.setSaveFormData(true);
+        s.setJavaScriptEnabled(true);
+
+        // enable navigator.geolocation
+        s.setGeolocationEnabled(true);
+        s.setGeolocationDatabasePath("/data/data/com.example.vimeotest/databases/");
+
+        // enable Web Storage: localStorage, sessionStorage
+        s.setDomStorageEnabled(true);
+
+        mContentView.addView(this);
+    }
+
+    public HTML5WebView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public HTML5WebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public HTML5WebView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public FrameLayout getLayout() {
+        return mLayout;
+    }
+
+    public boolean inCustomView() {
+        return (mCustomView != null);
+    }
+
+    public void hideCustomView() {
+        mWebChromeClient.onHideCustomView();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((mCustomView == null) && canGoBack()){
+                goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        private Bitmap 		mDefaultVideoPoster;
+        private View 		mVideoProgressView;
+
+        @Override
+        public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback)
+        {
+            Log.i(TAG, "here in on ShowCustomView");
+            Log.d(TAG, "View's class: "+view.getClass());
+
+            HTML5WebView.this.setVisibility(View.GONE);
+            Log.d(TAG, "HTML5WebView child count: "+HTML5WebView.this.getChildCount());
+
+            // if a view already exists then immediately terminate the new one
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+
+            mCustomViewContainer.addView(view);
+            mCustomView = view;
+            mCustomViewCallback = callback;
+            mCustomViewContainer.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            Log.d(TAG,"onHideCustomView");
+            if (mCustomView == null)
+                return;
+
+            // Hide the custom view.
+            mCustomView.setVisibility(View.GONE);
+
+            // Remove the custom view from its container.
+            mCustomViewContainer.removeView(mCustomView);
+            mCustomView = null;
+            mCustomViewContainer.setVisibility(View.GONE);
+            mCustomViewCallback.onCustomViewHidden();
+
+            HTML5WebView.this.setVisibility(View.VISIBLE);
+
+            Log.i(TAG, "set it to webVew");
+        }
+
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+            Log.i(TAG, consoleMessage.lineNumber()+" > "+consoleMessage.toString());
+            return true;
+        }
+
+        @Override
+        public Bitmap getDefaultVideoPoster() {
+            Log.i(TAG, "here in on getDefaultVideoPoster");
+            if (mDefaultVideoPoster == null) {
+                mDefaultVideoPoster = BitmapFactory.decodeResource(
+                        getResources(), R.drawable.default_video_poster);
+            }
+            return mDefaultVideoPoster;
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+            Log.i(TAG, "here in on getVideoLoadingPregressView");
+
+            if (mVideoProgressView == null) {
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+                mVideoProgressView = inflater.inflate(R.layout.video_loading_progress, null);
+            }
+            return mVideoProgressView;
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            Log.d(TAG,"onReceivedTitle");
+            ((Activity) mContext).setTitle(title);
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            Log.d(TAG, "onProgressChanged. progress: "+newProgress);
+            ((Activity) mContext).getWindow().setFeatureInt(Window.FEATURE_PROGRESS, newProgress*100);
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i(TAG, "shouldOverrideUrlLoading: "+url);
+            // don't override URL so that stuff within iframe can work properly
+            // view.loadUrl(url);
+            return false;
+        }
+
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            Log.d(TAG,"onLoadResource. url: "+url);
+            super.onLoadResource(view, url);
+        }
+
+
+    }
+
+    static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS =
+            new FrameLayout.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+}
--- app/src/main/java/org/gospelcoding/dailydoseofgreek/PlayEpisodeActivity.java
+++ app/src/main/java/org/gospelcoding/dailydoseofgreek/PlayEpisodeActivity.java
@@ -4,21 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class PlayEpisodeActivity extends AppCompatActivity {
 
-    WebView webView;
+    HTML5WebView webView;
     String vimeoUrl;
+
+    String html1 = "<!DOCTYPE html>" +
+                    "<html>" +
+                    "<head>" +
+                    "</head>" +
+                    "<body>" +
+		            "<div class=\"videoOut\">" +
+			        "<iframe id=\"player_1\" src=\"";
+    String html2 = "?title=0&amp;byline=0&amp;portrait=0&amp;color=c9ff23&amp;player_id=player_1\" width=\"100%\" height=\"50%\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen>" +
+	                "</iframe>" +
+		            "</div>" +
+	                "</body>" +
+                    "</html>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        webView = new WebView(this);
-        setContentView(webView);
+
         vimeoUrl = getIntent().getStringExtra(VideoListActivity.VIMEO_URL_EXTRA);
 
-        loadVideo();
+        webView = new HTML5WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.OFF);
+        webView.getSettings().setAllowFileAccess(true);
+
+        webView.loadData(html1 + vimeoUrl + html2, "text/html", null);
+
+        setContentView(webView.getLayout());
+
+        //loadVideo();
     }
 
     private void loadVideo(){
